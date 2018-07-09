6e88620c6e3dfe9616c5cf98453f9d13af18cc42
johnSjo <john.sjostrom@casumo.com>
2018-07-09 11:45:34 +0200

    adding a player module and some logic for it

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@
 import PubSub from './PubSub';
 
 import mainGame from './mainGame';
+import player from './player';
 
 const pubsub = PubSub.create();
 
 Promise.all([
     mainGame.init(pubsub),
+    player.init(pubsub),
 ])
     .then(() => {
         pubsub.publish('gameReady');
---
+++ src/player.js
@@ -0,0 +1,48 @@
+
+const LIVES = 3;
+
+export default {
+    init (pubsub) {
+
+        let lives;
+        const score = {
+            current: 0,
+            top: 0,
+            counter: null
+        };
+
+        pubsub.subscribe('treeHit', () => {
+            console.log('you hit a tree');
+
+            // update lives left
+            pubsub.publish('livesLeft', lives--);
+
+            if (lives < 1) {
+                // stop the score counter
+                clearInterval(score.counter);
+
+                // if dead trigger game over splash
+                pubsub.publish('allLivesSpent', score);
+            }
+        });
+
+        pubsub.subscribe('startGame', () => {
+            // reset 'lives'
+            lives = LIVES;
+
+            score.current = 0;
+
+            pubsub.publish('scoreUpdate', score);
+            pubsub.publish('livesLeft', lives);
+
+            // start counter
+            score.counter = setInterval(() => {
+                score.current++;
+                score.top = Math.max(score.current, score.top);
+
+                pubsub.publish('scoreUpdate', score);
+            }, 1000);
+
+        });
+    }
+};
